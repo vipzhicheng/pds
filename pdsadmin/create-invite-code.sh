f081584a93d60c4f6ca310a0c787fce2f32eddd8
Richard Yu <vipzhicheng@gmail.com>
2025-05-16 14:22:13 +0800

    fix: custom sh

--- pdsadmin/create-invite-code.sh
+++ pdsadmin/create-invite-code.sh
@@ -3,9 +3,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-PDS_ENV_FILE=${PDS_ENV_FILE:-"/pds/pds.env"}
+PDS_ENV_FILE=${PDS_ENV_FILE:-"/home/ubuntu/source/bluesky-selfhost-env/config/secrets-passwords.env"}
 source "${PDS_ENV_FILE}"
-
+PDS_HOSTNAME="pds.si46.world"
 curl \
   --fail \
   --silent \
